458ee43bdf02fa41e17fd6a443d5eb658a6dd055
Maksim Kostromin <daggerok@gmail.com>
2018-02-10 07:04:39 +0200

    Update event-store resource

--- src/main/java/daggerok/chat/queries/EventsProjection.kt
+++ src/main/java/daggerok/chat/queries/EventsProjection.kt
@@ -1,19 +1,26 @@
 package daggerok.rest
 
 import com.mongodb.MongoClient
+import org.axonframework.eventsourcing.eventstore.EventStore
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.format.annotation.DateTimeFormat.ISO.DATE_TIME
 import org.springframework.http.HttpStatus.OK
 import org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.stream.Collectors
 
 @RestController
-class EventsProjection(val mongoClient: MongoClient,
+class EventsProjection(val eventStore: EventStore,
+                       val mongoClient: MongoClient,
                        @Value("\${spring.datasource.name}") val name: String) {
+
+  @GetMapping("/{aggregateId}")
+  fun queryEvents(@PathVariable aggregateId: String) =
+      eventStore
+          .readEvents(aggregateId)
+          .asStream()
+          .collect(Collectors.toList())
 
   @ResponseStatus(OK)
   @DateTimeFormat(iso = DATE_TIME)
